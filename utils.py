42e7fc4e8edb5605c832c2a21bf9f5766de08796
RuslanUsmanov <usmanovruslan322@gmail.com>
2023-11-17 00:23:00 +0500

    fix utils

--- utils.py
+++ utils.py
@@ -19,15 +19,15 @@ def get_interfaces() -> list[str]:
     command = "ip link list"
     output = subprocess.check_output(command, shell=True).decode()
     link_pattern = r'\d{,2}: [a-z0-9]+'
-    print(output)
     matches = re.findall(link_pattern, output)
 
     return [link[1] for link in [m.split() for m in matches]]
+
 
 def get_ips() -> dict[str, str]:
     ips = {}
     links = get_interfaces()
     for link in links:
-        if link[0] == 'e':
+        if link != 'lo':
             ips[link] = get_interface_ip(link)
     return ips
